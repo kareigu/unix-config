e119b42a1f74339814f6dfc52e48651c19a05dad
karei <eetu.kivelae@gmail.com>
2024-02-25 18:45:42 +0200

    Update neovide options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -18,5 +18,11 @@ append_val(opt.winbl, 25)
 append_val(opt.pumblend, 25)
 
 if vim.g.neovide then
-  vim.opt.guifont = "SF Mono:h12"
+  vim.opt.guifont = "Mononoki Nerd Font Mono:h13"
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_transparency = 0.7
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
 end
