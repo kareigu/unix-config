c3c9f5e2bd80a2000512228b5ca320bb1ffc4e78
karei <eetu.kivelae@gmail.com>
2024-04-07 17:37:46 +0300

    Fix server configuration being at the wrong key

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -2,11 +2,11 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = true },
-  },
-  ---@type lspconfig.options
-  servers = {
-    clangd = {
-      mason = false,
+    ---@type lspconfig.options
+    servers = {
+      clangd = {
+        mason = false,
+      },
     },
   },
 }
