f7788bada5341e5fcc208832fa64cc03d941dbf3
karei <mail@karei.dev>
2024-08-14 16:50:05 +0300

    Remove dressing

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -15,22 +15,6 @@ return {
     end,
   },
   {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
-  {
     "julienvincent/hunk.nvim",
     dependencies = {
       "MunifTanjim/nui.nvim",
