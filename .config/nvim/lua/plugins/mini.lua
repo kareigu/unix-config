1d50515b139ea3d6589e4f2955c23d3ab0783ef6
karei <mail@karei.dev>
2024-11-28 20:26:36 +0200

    nvim: change `mini.surround` settings

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -24,14 +24,24 @@ return {
     event = "VeryLazy",
     opts = {
       mappings = {
-        add = "ms",
-        delete = "md",
-        replace = "mr",
-        highlight = "mh",
-        find = "mf",
-        find_left = "mF",
-        update_lines = "mn",
+        add = "ys",
+        delete = "ds",
+        replace = "cs",
+        highlight = "sh",
+        find = "sf",
+        find_left = "sF",
+        update_lines = "sn",
+        suffix_last = "",
+        suffix_next = "",
       },
+      search_method = "cover_or_next",
+      custom_surroundings = {
+        ["B"] = { output = { left = "{", right = "}" } },
+      },
+    },
+    keys = {
+      { "S", "ys", desc = "surround selection", remap = true, mode = "x" },
+      { "yss", "ys_", desc = "surround line", remap = true },
     },
   },
 }
