0cc498697e5feb8371c99df10b57a8962fcf67a7
karei <mail@karei.dev>
2025-12-16 09:13:16 +0200

    nvim: remove `mini.files`

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,25 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "echasnovski/mini.files",
-    version = false,
-    opts = {
-      windows = { preview = true, width_preview = 80 },
-      options = {
-        use_as_default_explorer = false,
-      },
-    },
-    keys = {
-      {
-        "<leader>f",
-        function()
-          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-        end,
-        desc = "Open mini files",
-      },
-    },
-  },
-  {
     "echasnovski/mini.surround",
     event = "VeryLazy",
     opts = {
